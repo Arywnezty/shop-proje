650d4c1bd99320b8e20889902fc03060a0f35451
Arywnezty <ezzatiaryan13@gmail.com>
2025-06-06 12:54:00 +0330

    add select page electronic

--- src/style.js
+++ src/style.js
@@ -33,7 +33,6 @@ async function handleGetMenpage(){
 }
 handleGetMenpage();
 
-
 function getdataWomen(){
  const result = fetch("https://fakestoreapi.com/products/category/women's clothing")
   .then(res => res.json())
@@ -87,6 +86,35 @@ async function handleGetjewelerypage(){
   const data= await getdatajewelery();
   renderbodypagejewelery(data);
 }
+handleGetjewelerypage();
+
+function getdataelectronic(){
+  const result = fetch("https://fakestoreapi.com/products/category/electronics")
+  .then(res => res.json())
+  .catch(err =>Toastify({
+    text: "اطلاعات شما دارای خطا می باشد!",
+    duration: 3000,
+    // destination: "https://github.com/apvarun/toastify-js",
+    // newWindow: true,
+    close: true,
+    gravity: "top", // `top` or `bottom`
+    position: "center", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: "linear-gradient(to right, #ff5555, #ff0000)",
+    },
+    onClick: function(){
+      location.reload()
+    } // Callback after click
+  }).showToast());
+  
+  return result;
+}
+async function handleGetelectronicpage(){
+  const data= await getdataelectronic();
+  renderbodypageelectronic(data);
+}
+handleGetelectronicpage();
 
 
 function handleMenclick(evt){
@@ -118,6 +146,17 @@ function handlejeweleryclick(evt){
   
   checkstate();
 }
+
+function handleelectronicclick(evt){
+  evt.preventDefault();
+  
+  const href=evt.target.getAttribute("href")
+  
+  history.pushState({},"",href);
+  
+  checkstate();
+}
+
 
 function renderbodypageman(items){
   let menClothes=document.getElementById("men-clothes");
@@ -154,7 +193,6 @@ Toastify({
     onClick: function(){} // Callback after click
   }).showToast();
 }
-
 
 function renderbodypagewoman(items){
     const womenClothes=document.getElementById("women-clothes");
@@ -199,6 +237,28 @@ function renderbodypagejewelery(items){
   }).join("");
   
   jewelery.innerHTML=template;
+}
+
+function renderbodypageelectronic(items){
+  const electronic=document.getElementById("electronic");
+  const template=items.map(item=>{
+  return `
+  <div class="w-full max-w-[65rem] flex flex-col gap-4 bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
+    <img 
+      src="${item.image}" 
+      alt="${item.title}" 
+      class="w-full h-48 object-contain"
+    >
+    <div class="p-4 flex flex-col gap-2">
+    <p class="text-gray-400">${item.category}</p>
+      <h2 class="text-lg font-medium text-gray-900 line-clamp-2">${item.title}</h2>
+      <span class="text-lg font-bold text-indigo-600">${item.price} تومان$</span>
+    </div>
+  </div>
+  `
+  }).join("");
+  
+  electronic.innerHTML=template;
 }
 
 function rendermenpage(){
@@ -426,6 +486,81 @@ function renderjewelerypage(){
     root.innerHTML=container;
   }
 }
+
+function renderelectronicpage(){
+  root.innerHTML=`
+  <div class="grid grid-cols-1 gap-4 md:grid-cols-4 px-4 rounded-md mt-5">
+  <div class="w-full h-96 p-1 bg-slate-200 animate-pulse">
+     <div class="w-full bg-slate-300 h-64">
+     </div>
+     <div class="w-1/5 h-6 rounded-lg bg-slate-300 mt-4"></div>
+     <div class="w-1/7 h-6 rounded-lg bg-slate-300 mt-4"></div>
+     <div class="w-1/3 h-6 rounded-lg bg-slate-300 mt-4"></div>
+  </div>
+  <div class="w-full h-96 p-1 bg-slate-200 animate-pulse">
+   <div class="w-full bg-slate-300 h-64">
+   </div>
+   <div class="w-1/5 h-6 rounded-lg bg-slate-300 mt-4"></div>
+   <div class="w-1/7 h-6 rounded-lg bg-slate-300 mt-4"></div>
+   <div class="w-1/3 h-6 rounded-lg bg-slate-300 mt-4"></div>
+  </div>
+  <div class="w-full h-96 p-1 bg-slate-200 animate-pulse">
+  <div class="w-full bg-slate-300 h-64">
+  </div>
+  <div class="w-1/5 h-6 rounded-lg bg-slate-300 mt-4"></div>
+  <div class="w-1/7 h-6 rounded-lg bg-slate-300 mt-4"></div>
+  <div class="w-1/3 h-6 rounded-lg bg-slate-300 mt-4"></div>
+  </div>
+  <div class="w-full h-96 p-1 bg-slate-200 animate-pulse">
+  <div class="w-full bg-slate-300 h-64">
+  </div>
+  <div class="w-1/5 h-6 rounded-lg bg-slate-300 mt-4"></div>
+  <div class="w-1/7 h-6 rounded-lg bg-slate-300 mt-4"></div>
+  <div class="w-1/3 h-6 rounded-lg bg-slate-300 mt-4"></div>
+  </div>
+  </div>
+  `
+  fetch("https://fakestoreapi.com/products/category/electronics")
+  .then(res => res.json())
+  .then(json=>renderelectronicdata(json))
+  .catch(err =>{Toastify({
+    text: "اطلاعات شما دارای خطا می باشد!",
+    duration: 3000,
+    destination: "https://github.com/apvarun/toastify-js",
+    newWindow: true,
+    close: true,
+    gravity: "top", // `top` or `bottom`
+    position: "center", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: "linear-gradient(to right, #ff5555, #ff0000)",
+    },
+    onClick: function(){
+      location.reload()
+    } // Callback after click
+  }).showToast()});
+  function renderelectronicdata(data){
+    const template=data.map(item =>{
+      return`
+      <div class="w-full max-w-[65rem] flex flex-col gap-4 bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
+      <img 
+        src="${item.image}" 
+        alt="${item.title}" 
+        class="w-full h-48 object-contain"
+      >
+      <div class="p-4 flex flex-col gap-2">
+      <p class="text-gray-400">${item.category}</p>
+        <h2 class="text-lg font-medium text-gray-900 line-clamp-2">${item.title}</h2>
+        <span class="text-lg font-bold text-indigo-600">${item.price} تومان$</span>
+      </div>
+    </div>
+      `
+    }).join("");
+    
+    const container=`<div class="grid grid-cols-1 md:grid-cols-4 gap-4 container mt-5">${template}</div>`
+    root.innerHTML=container;
+  }
+}
 async function renderMainpage(){
   const Template= ` 
   <div class="container-lg hidden lg:block">
@@ -583,6 +718,9 @@ renderbodypagewoman(Womendata);
 
 const jewelerydata = await getdatajewelery();
 renderbodypagejewelery(jewelerydata);
+
+const electronicdata = await getdataelectronic();
+renderbodypageelectronic(electronicdata);
 }
 
 
@@ -599,6 +737,9 @@ switch (url) {
       break;
       case "/jewelery":
       renderjewelerypage();
+      break;
+      case "/electronic":
+      renderelectronicpage();
       break;
   default:renderMainpage();
     break;
